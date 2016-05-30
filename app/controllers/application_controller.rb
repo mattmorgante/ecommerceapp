f5aa6937b6d5bdb4314a5bbc917c74db83e3126f
Matt <matthewmorgante@gmail.com>
2016-05-30 13:33:25 +0200

    fixed homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,6 @@ class ApplicationController < ActionController::Base
       Order.new 
     end 
   end 
+
+  # test
 end
--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -8,14 +8,14 @@ class OrderItemsController < ApplicationController
 
   def update
     @order = current_order
-    @order_item = @order.order_items.find(prams[:id])
+    @order_item = @order.order_items.find(params[:id])
     @order_item.update_attributes(order_item_params)
     @order_items = @order.order_items
   end
 
   def destroy
     @order = current_order
-    @order_item = @order.order_items.find(prams[:id])
+    @order_item = @order.order_items.find(params[:id])
     @order_item.destroy
     @order_items = @order.order_items
   end
